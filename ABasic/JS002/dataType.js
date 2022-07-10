522330852ef0b9da003c274aa722e020386755c5
ilikui <2563043733@qq.com>
2022-07-10 20:26:15 +0800

    :smile:

---
+++ ABasic/JS002/dataType.js
@@ -0,0 +1,23 @@
+
+
+let a = 12;
+
+let b = 'H';
+
+let c = "Hello world";
+
+
+let d = true;
+
+
+
+
+console.log(typeof(a));
+
+
+console.log(typeof(b));
+
+
+console.log(typeof(c));
+
+console.log(typeof(d));
