510fee7eaa32320238ca191cef1d291fec305a9c
ilikui <2563043733@qq.com>
2022-07-10 19:07:01 +0800

    update

---
+++ ABasic/JS004/FunJs.js
@@ -0,0 +1,15 @@
+/**
+ * 函数返回值
+ * @param {x} x 
+ * @returns 
+ */
+let squre = function(x)
+{
+    return x *x;
+}
+
+
+
+console.log("打印输出");
+
+console.log(squre(5));
---
+++ ABasic/JS004/String.js
@@ -0,0 +1,9 @@
+let str = "Hello world";
+
+
+let size = str.length;
+
+
+
+
+console.log("字符串 str的长度：",size);
